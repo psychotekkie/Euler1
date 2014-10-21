cfb5b907e2dc23dec77decfa22c4f7a0b136e522
psychotekkie <dfrench@hwdsb.on.ca>
2014-10-21 00:08:11 -0400

    Euler Problem 1 in Visual Basic.Net

---
+++ Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMultiples = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMultiples
+        '
+        Me.btnMultiples.Location = New System.Drawing.Point(0, 0)
+        Me.btnMultiples.Name = "btnMultiples"
+        Me.btnMultiples.Size = New System.Drawing.Size(281, 23)
+        Me.btnMultiples.TabIndex = 0
+        Me.btnMultiples.Text = "Find Multiples of 3 and 5"
+        Me.btnMultiples.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 26)
+        Me.Controls.Add(Me.btnMultiples)
+        Me.Name = "Form1"
+        Me.Text = "Euler1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnMultiples As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿REM  D. French
+REM  October 18, 2014
+REM  Project Euler Problem #1
+
+Public Class Form1
+    Private Sub btnMultiples_Click(sender As Object, e As EventArgs) Handles btnMultiples.Click
+        Dim num As Integer
+        Dim total As Integer
+        num = 1
+        Do While num < 1000
+            If (num Mod 3 = 0) Or (num Mod 5 = 0) Then
+                total += num
+            End If
+            num += 1
+        Loop
+        MsgBox(total)
+    End Sub
+End Class
